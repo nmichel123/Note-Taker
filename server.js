43ac1cbdc88017c94c880a4ab65dd8f0f342b09a
Nico Michel <nicomichel@utexas.edu>
2020-07-27 20:25:05 -0500

    deleted package lock and shifted server and packages to root level

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-require("./routes/apiRoutes")
+require("./Develop/routes/apiRoutes")
 
 app.listen(PORT, function (){
     console.log ("App listening on: http://localhost:" + PORT )
