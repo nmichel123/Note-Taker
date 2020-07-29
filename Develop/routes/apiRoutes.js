a31933739c32d7359df8a21a2e589be3de69bcaa
Nico Michel <nicomichel@utexas.edu>
2020-07-29 01:21:25 -0500

    working on routes, will find a way to make successful post call tomorrow

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,20 @@
+var fs = require("fs")
 var note = require("../db/db.json")
 
+module.exports = function (app) {
 
+    app.get("/api/notes", function (req, res) {
+        res.json(note);
+    }); 
+
+
+
+    app.post("/api/notes", function (req, res){
+        
+    });
+
+
+    app.post("/api/clear", function (req, res){
+        destroy(note)
+    })
+};
