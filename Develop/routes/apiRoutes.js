8ebe5755af68c1ca2bdacf612c52240cf822ca7c
Nico Michel <nicomichel@utexas.edu>
2020-07-30 19:18:21 -0500

    I thought this would work next few pushes should

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,20 +1,22 @@
-var fs = require("fs")
-var note = require("../db/db.json")
+var fs = require("fs");
+var util = require("util");
+var note = require("../db/db.json");
 
 module.exports = function (app) {
 
     app.get("/api/notes", function (req, res) {
+        fs.readFile("../db/db.json")
         res.json(note);
     }); 
 
-
-
-    app.post("/api/notes", function (req, res){
-        
+    app.post("/api/notes", function (req, res) {
+        const userNote = {};
+        userNote.body = req.body.newNote
+        note.push(userNote);
+        res.redirect('/');
     });
 
-
     app.post("/api/clear", function (req, res){
-        destroy(note)
+        destroy(note).replace(/\s+/g, "").toLowerCase();
     })
 };
