0e29bfcad0f951a522534481abb117a82540f180
Nico Michel <nicomichel@utexas.edu>
2020-08-14 19:12:46 -0500

    trying to fix heroku upload

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require("express");
-var bodyParser = require("body-parser")
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("./Develop/public/assets"));
 
-require("./Develop/routes/apiRoutes");
-require("./Develop/routes/htmlRoutes");
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 
 app.listen(PORT, function (){
