825220fc452b5defb4f0430e019e967c93c17865
Nico Michel <nicomichel@utexas.edu>
2020-11-10 18:56:21 -0600

    fixed store logic

--- db/store.js
+++ db/store.js
@@ -11,4 +11,48 @@ class Store {
     read() {
         return readFileAsync("db/db.json", "utf8");
     }
+
+    write() {
+        return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            // If notes is not an array or cannot be turned into one, send back a fresh empty array
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank")
+        }
+
+        // Add a unique id using uuid package
+        const newNote = { title, text, id: uuidv1() };
+
+        // Get all notes, add the new note, write all updated notes, return the newNote
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+
+    removeNote(id) {
+        // Get all notes, remove the note with the given id, write the filtered notes
+        return this.getNotes()
+            .then((notes) => notes.filter((note) => note.id !==id))
+            .then((filteredNotes) => this.write(filteredNotes));
+    }
 }
+
+module.exports = new Store();
